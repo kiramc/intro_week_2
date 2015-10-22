fdb6a3626a7338cc5fac88a5686ce557661e5345
Kira McLean <kiraemclean@gmail.com>
2015-10-22 13:54:03 -0400

    Complete intermediate OOP quiz

--- int_quiz.rb
+++ int_quiz.rb
@@ -65,6 +65,145 @@ end
 # you also need to add self. in front of quantity to access the varible
 
 
+# [4]
+
+class Greeting
+  def print_string(string)
+    puts string
+  end
+end
+
+class Hello < Greeting
+  def hi
+    print_string("Hello!")
+  end
+end
+
+class Goodbye < Greeting
+  def bye
+    print_string("Goodbye!")
+  end
+end
+
+# [5]
+
+class KrispyKreme
+  attr_reader :filling_type, :glazing
+
+  def initialize(filling_type, glazing)
+    @filling_type = filling_type
+    @glazing = glazing
+  end
+
+  def to_s
+    filling_string = filling_type ? filling_type : "Plain"
+    glazing_string = glazing ? " with #{glazing}" : ""
+    filling_string + glazing_string
+  end        
+end
+
+donut1 = KrispyKreme.new(nil, nil)
+donut2 = KrispyKreme.new("Vanilla", nil)
+donut3 = KrispyKreme.new(nil, "sugar")
+donut4 = KrispyKreme.new(nil, "chocolate sprinkles")
+donut5 = KrispyKreme.new("Custard", "icing")
+
+puts donut1
+puts donut2
+puts donut3
+puts donut4
+puts donut5
+
+# [6]
+
+class Computer
+  attr_accessor :template
+
+  def create_template
+    @template = "template 14321"
+  end
+
+  def show_template
+    template
+  end
+end
+
+class Computer
+  attr_accessor :template
+
+  def create_template
+    self.template = "template 14321"
+  end
+
+  def show_template
+    self.template
+  end
+end
+
+#no difference, and self. is not required
+
+# [7]
+
+class Light
+  attr_accessor :brightness, :color
+
+  def initialize(brightness, color)
+    @brightness = brightness
+    @color = color
+  end
+
+  def self.light_information
+    "I want to turn on the light wiht a brightness level of super high and a color of green"
+  end
+end
+
+# reference variables, don't hard code values. Also don't need self. so:
+
+class Light
+  attr_accessor :brightness, :color
+
+  def initialize(brightness, color)
+    @brightness = brightness
+    @color = color
+  end
+
+  def information
+    "I want to turn on the light wiht a brightness level of #{brightness} and a color of #{color}"
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
